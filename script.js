aa61175efae0816ea467a13c09c10adafae52313
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-30 09:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let line = document.getElementById('line');
+let deg = 2;
+function rotate(){
+	line.style.transform = `rotate(${deg}deg)`;
+	deg += 2;
+}
+setInterval(rotate,200);
